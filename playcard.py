7ed248407bf86bb19634960fcf5a86809d668436
sansanaye99 <93754734+sansanaye99@users.noreply.github.com>
2024-07-28 23:08:20 +0630

    Update playcard.py

--- playcard.py
+++ playcard.py
@@ -1,5 +1,7 @@
 import streamlit as st
 a=st.text_input("please enter your name")
-st.write(a)
-st.write("This is playcard")
+b=st.text_input("please enter your age")
+st.write("Your name is"+a)
+st.write("Your age is"+b)
+
 
