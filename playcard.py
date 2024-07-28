d530d948c0b5e7692086b610c4352f4b5b6493b0
sansanaye99 <93754734+sansanaye99@users.noreply.github.com>
2024-07-28 23:14:04 +0630

    Update playcard.py

--- playcard.py
+++ playcard.py
@@ -1,10 +1,14 @@
 import streamlit as st
 import test
 a=st.text_input("please enter your name")
-b=st.text_input("please enter your age")
-st.write("Your name is"+a)
-st.write("Your age is"+b)
-test.display(a)
+b=int(st.text_input("please enter your age"))
+if (b<=18):
+  st.write("you are not allow to access the site")
+else:
+  st.write("You are eligible to play the game")
+  st.write("Your name is"+a)
+  st.write("Your age is"+b)
+  test.display(a)
 
 
 
