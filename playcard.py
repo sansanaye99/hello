9acd37f9d544ea666b8c9dc16e45a849cd89e6c3
sansanaye99 <93754734+sansanaye99@users.noreply.github.com>
2024-07-28 22:50:56 +0630

    Update playcard.py

--- playcard.py
+++ playcard.py
@@ -1,6 +1,6 @@
 import streamlit as st
-speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
-for i in speed:
+a=input("please enter your name")
+for i in a:
   st.write(i)
 st.write("This is playcard")
 
